412e43553ce5a9ff3855bbf427ea05aef2d24b43
Dan Green <danngreen1@gmail.com>
2022-08-25 22:18:12 -0700

    Removed unused files

--- build.sh
+++ build.sh
@@ -1,28 +1,19 @@
 rm -rf build
 mkdir -p build
 
-arm-none-eabi-g++ -O0 -g2 \
+arm-none-eabi-g++ -O0 \
 	-mcpu=cortex-m0 -march=armv6-m -mlittle-endian \
-	-fno-common -fdata-sections -ffunction-sections \
 	-fno-unwind-tables -fno-exceptions \
 	-ffreestanding -fno-rtti -fno-threadsafe-statics \
 	-nostdlib -nostartfiles \
-	-Wall \
 	-c main.cc -o build/main.o
-
-arm-none-eabi-g++ -O0 -g2 \
-	-mcpu=cortex-m0 -march=armv6-m -mlittle-endian \
-	-fno-common -fdata-sections -ffunction-sections \
-	-fno-unwind-tables -fno-exceptions \
-	-ffreestanding -fno-rtti -fno-threadsafe-statics \
-	-nostdlib -nostartfiles \
-	-Wall \
-	-c startup.s -o build/startup.o
 
 arm-none-eabi-g++ \
 	-Wl,--gc-sections \
 	-Wl,-Map,build/test.map,--cref \
 	-mcpu=cortex-m0 -march=armv6-m -mlittle-endian \
 	-T link.ld \
-	-nostdlib -nostartfiles -ffreestanding  \
-	-o build/test.elf build/main.o build/startup.o 
+	-nostdlib -nostartfiles \
+	-o build/test.elf build/main.o
+
+arm-none-eabi-size build/test.elf
--- main.cc
+++ main.cc
@@ -1,4 +1,8 @@
-void main() {}
+extern "C" void __libc_init_array();
+
+extern "C" void Reset_Handler() { 
+	__libc_init_array();
+}
 
 // Stubs:
 extern "C" void _init() {}
@@ -7,4 +11,6 @@ extern "C" void *memcpy(void *dst, const void *src, unsigned n) {
   return nullptr;
 }
 
-// char __aeabi_unwind_cpp_pr0[0];
+// Compiling with this commented out creates a ~4kB .text section
+// Compiling with this uncommented creates a <200B .text section
+char __aeabi_unwind_cpp_pr0[0];
