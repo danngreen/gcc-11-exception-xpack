d03daa018619554a33200a83c11f54a79410ec68
Dan Green <danngreen1@gmail.com>
2022-08-25 22:20:25 -0700

    Default to non defining __aeabi_unwind_cpp_pr0

--- main.cc
+++ main.cc
@@ -13,4 +13,4 @@ extern "C" void *memcpy(void *dst, const void *src, unsigned n) {
 
 // Compiling with this commented out creates a ~4kB .text section
 // Compiling with this uncommented creates a <200B .text section
-char __aeabi_unwind_cpp_pr0[0];
+// char __aeabi_unwind_cpp_pr0[0];
